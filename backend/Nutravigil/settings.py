94e847ed94ea063c1e11cc2dc69f1808a3f45a21
RayFR <freddierayc@gmail.com>
2024-09-26 22:45:29 +0100

    setup mysql and all envs

--- backend/Nutravigil/settings.py
+++ backend/Nutravigil/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'myapp',
 ]
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Nutravigil.urls'
@@ -78,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nutravigil_db',
-        'USER': 'Fred',
+        'USER': 'fred',
         'PASSWORD': '1144',
         'HOST': 'localhost',
         'PORT': '3306',
@@ -127,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CUSTOM
+
+CORS_ALLOW_ALL_ORIGINS = True
