477391b06e3f86ba6af5cab8da2501013e1bee85
RayFR <freddierayc@gmail.com>
2025-03-16 16:58:29 +0000

    login/2 + signup/2 + get_user + logout

--- backend/nutravigil/nutravigil/routes/auth.py
+++ backend/nutravigil/nutravigil/routes/auth.py
@@ -3,7 +3,27 @@ from nutravigil.models import db
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/")
-def home():
-    return "FLASK APP WORKING"
+@auth.route("/get_user/<int:id>", methods['GET'])
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify({
+        "userId": user.id,
+        "username": user.username,
+        "email": user.email,
+        "password": user.password
+    })
+
+@auth.route("/signup", methods=['POST'])
+def signup():
+    pass
+
+@auth.route("/login", methods=['POST'])
+def login():
+    pass
+
+@auth.route("/logout", methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"success -> user logged out": True}), 201
 
